20cacb17873409bfa7e0da78f270d06a99cdcb22
Lutain-Brogy <brogylutain@gmail.com>
2026-06-18 04:50:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,21 @@ import streamlit as st
 
 st.title('Supabase Rulesense')
 
-user_input = st.text_input("")
+choice = st.selectbox(
+    "Choose the rule type you want",
+    ["Read", "Edit/Delete"]
+)
 
-mode = 'rule'
-else:
-mode = 'normal'
+
+if choice == "Read":
+    A = st.text_input("The user's UI (user_id)")
+    B = st.text_input("Rule name")
+    C = st.text_input("Which table")
+
+    st.code(f"""
+CREATE POLICY "{B}"
+ON public."{C}"
+FOR SELECT
+TO authenticated
+USING (auth.uid() = '{A}');
+""")
