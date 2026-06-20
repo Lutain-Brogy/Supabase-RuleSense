2cb343fac8dbceec9ef0807bd0708902740ab333
Lutain-Brogy <brogylutain@gmail.com>
2026-06-20 06:42:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,46 +1,61 @@
 import streamlit as st
 
 st.title('Supabase Rulesense')
-'''
-choice = st.selectbox(
-    "Choose the rule type you want",
-    ["Read", "Edit/Delete"]
-)
 
+# choice = st.selectbox(
+#     "Choose the rule type you want",
+#     ["Read", "Edit/Delete"]
+# )
+#
+#
+# if choice == "Read":
+#     A = st.text_input("Rule name")
+#     B = st.text_input("Which table is affected") 
+#     C = st.selectbox(
+#     'For',
+#     ["Anyone logged in", "One certain person"]
+# )
+#     if C == 'Anyone logged in':
+#         A = st.text_input('Name/description')
+#         B = st.text_input('Name of table')
+#         st.code(f'''
+# CREATE POLICY "{A}"
+# ON public."{B}"
+# FOR SELECT
+# TO authenticated
+# USING (true);
+# ''') 
+#
+#     if C == 'One certain person':
+#         A = st.text_input('Policy name/role')
+#         B = st.text_input('Table name')
+#         C = st.text_input('User ID')
+#         st.code(f'''
+# CREATE POLICY "{A}"
+# ON public."{B}"
+# FOR SELECT
+# TO authenticated
+# USING (auth.uid() = "{C}");
+# ''')
+user_input = st.text_input
 
-if choice == "Read":
-    A = st.text_input("Rule name")
-    B = st.text_input("Which table is affected") 
-    C = st.selectbox(
-    'For',
-    ["Anyone logged in", "One certain person"]
-)
-    if C == 'Anyone logged in':
-        A = st.text_input('Name/description')
-        B = st.text_input('Name of table')
-        st.code(f'''
+tab1, tab2, tab3 = st.tabs("
+                            ["Read", "Insert", "Update"]
+                         ")
+
+with tab1:
+    Table_type = st.selectbox('Select ready type'
+                              ["Anyone logged in", "Certain person"] 
+                             ')
+     if table_type = 'Anyone logged in':
+    A = st.text_input('Policy name')
+    B = st.text_input('Table name')
+         st.code(f'''
 CREATE POLICY "{A}"
 ON public."{B}"
 FOR SELECT
 TO authenticated
 USING (true);
-''') 
-
-    if C == 'One certain person':
-        A = st.text_input('Policy name/role')
-        B = st.text_input('Table name')
-        C = st.text_input('User ID')
-        st.code(f'''
-CREATE POLICY "{A}"
-ON public."{B}"
-FOR SELECT
-TO authenticated
-USING (auth.uid() = "{C}");
-''')
-''' 
-
-
-
-
+''')          
 
 
