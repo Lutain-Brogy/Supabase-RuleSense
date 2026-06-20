9397270d152602fad38a0ec78719fcc08bdae079
Lutain-Brogy <brogylutain@gmail.com>
2026-06-20 11:41:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import streamlit as st
 
 st.title('Supabase Rulesense')
+user_input = st.text_input
+
 
 mode = st.selectbox('What would you like to do today'
                     ["Write policy", "Ask questiond"]
                    )
+
 if mode == 'What would you like to do today':
 
   Rule_type = st.selectbox('Choose the required type of policy'
