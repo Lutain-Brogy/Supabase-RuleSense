e9baac839b9bc31920bb90619ba4d1eebe955c6e
Lutain-Brogy <brogylutain@gmail.com>
2026-06-20 11:49:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ st.title('Supabase Rulesense')
 mode = st.selectbox("What would you like to do today", ["Write policy", "Ask questions"])
 
 if mode == 'Write policy':
-
-  Rule_type = st.selectbox('Choose the required type of policy'
-                           [ "SELECT",  "INSERT", "UPDATE", "DELETE","ALL", "REFERENCES" , "TRIGGER" ]
-                          )
+  Rule_type = st.selectbox(
+    "Choose the required type of policy",
+    ["SELECT", "INSERT", "UPDATE", "DELETE", "ALL", "REFERENCES", "TRIGGER"]
+    )
+  
   
         
        
