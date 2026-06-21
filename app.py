328ce67b600a63bed09419af4d4d66e9db390cc4
Lutain-Brogy <brogylutain@gmail.com>
2026-06-21 04:41:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,8 @@ with start1:
 
    if write_type == 'Straightforward':
    
-  Rule_type = st.selectbox("Choose the required type of policy",["SELECT", "INSERT", "UPDATE", "DELETE", "ALL", "REFERENCES", "TRIGGER"])
+Rule_type = st.selectbox("Choose the required type of policy",
+                           ["SELECT", "INSERT", "UPDATE", "DELETE", "ALL", "REFERENCES", "TRIGGER"])
   
   if Rule_type == 'SELECT':
      st.write('Which type of read policy sir/madam?')
