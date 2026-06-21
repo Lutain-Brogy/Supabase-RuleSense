d0812ef30829b12a84b55a4b62bbea96b47507e2
Lutain-Brogy <brogylutain@gmail.com>
2026-06-21 05:02:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import streamlit as st
 
 st.title('Supabase Rulesense')
 
-
-start1, #start2, start3
-= st.tabs(["Write policy"])# "Ask questions" , "Smart policy generating"])
+ #start2, start3
+start1 = st.tabs(["Write policy"])# "Ask questions" , "Smart policy generating"])
    
 with start1:
     write_type = st.selectbox(
