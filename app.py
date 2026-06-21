3c4125a14843d35fcc3570749a96801857fa4b63
Lutain-Brogy <brogylutain@gmail.com>
2026-06-21 04:38:21 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,13 +3,22 @@ import streamlit as st
 st.title('Supabase Rulesense')
 
 
-mode = st.selectbox("What would you like to do today", ["Write policy", "Ask questions"])
+ start1, start2, start3 = st.tabs(["Write policy", "Ask questions" , "Smart policy generating"])
+ with start2:
+   user_input = st.text_input
+   
+ with start1:
+   write_type = st.selectbox("Explanatory", "Straightforward"])
+   if write_type == 'Explanatory':
+     user_input = st.text_input
 
-if mode == 'Write policy':
+   if write_type == 'Straightforward':
+   
   Rule_type = st.selectbox(
     "Choose the required type of policy",
     ["SELECT", "INSERT", "UPDATE", "DELETE", "ALL", "REFERENCES", "TRIGGER"]
   )  
+
   
   if Rule_type == 'SELECT':
      st.write('Which type of read policy sir/madam?')
@@ -44,7 +53,8 @@ FOR SELECT
 TO authenticated
 USING (auth.uid() = '{D}');
 ''')
-    
+
+ 
 
 
 
